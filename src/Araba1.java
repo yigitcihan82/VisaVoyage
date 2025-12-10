23e3ff0efd11132d1a898b64264bf9962830ca25
Emirhan <emirhanuzun936@gmail.com>
2025-12-10 18:03:46 +0300

    araba classı

---
+++ src/Araba1.java
@@ -0,0 +1,14 @@
+public class Araba1 {
+
+    public int plate;
+    public String fuelType;
+
+
+     public Araba1(int plate, String fuelType ){
+        this.fuelType = fuelType;
+        this.plate = plate;
+    }
+
+
+
+}

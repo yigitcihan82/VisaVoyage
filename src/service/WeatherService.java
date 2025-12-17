6e82735bba02c224e4864463e66a4530f27de09a
batik <batikanavsar000@gmail.com>
2025-12-17 22:34:41 +0300

    f

--- src/service/WeatherService.java
+++ src/service/WeatherService.java
@@ -43,3 +43,4 @@ public class WeatherService {
                 "\nAsistan Tavsiyesi: " + advice;
     }
 }
+//SDVNSKDJPSVJPSDVJPSJVSPD
--- src/ui/ConsoleMenu.java
+++ src/ui/ConsoleMenu.java
@@ -83,3 +83,4 @@ public class ConsoleMenu {
         new ConsoleMenu().start();
     }
 }
+//ZHZVHIVHSIOGDHSOIHSOI
